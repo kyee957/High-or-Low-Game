c7e39d4964499149de441f7189a4ef7148723fbc
kyee957 <kyee957@gmail.com>
2020-03-18 09:01:44 -0500

    outline

---
+++ js/app.js
@@ -0,0 +1,271 @@
+// Each of the two players are given 3 cards from a standard deck and a deck of cards
+
+
+// Players must decide if their mystery card will be higher or lower 
+
+// Players take turn when a player loses/wins a round. 
+
+// If a player guesses incorrectly, they must restart the round they are currently in. 
+
+// If a player guesses correctly on all 3 cards show on the table, 
+// they will go onto the next round. 
+
+// Game ends when a player is able to pass a total of 3 rounds.
+
+
+// create standard deck of cards
+const myDeck = [
+  {
+    name: "spades-two", //spades
+    value: 2
+  }, {
+    name: "spades-three",
+    value: 3
+  }, {
+    name: "spades-four",
+    value: 4
+  }, {
+    name: "spades-five",
+    value: 5
+  }, {
+    name: "spades-six",
+    value: 6
+  }, {
+    name: "spades-seven",
+    value: 7
+  }, {
+    name: "spades-eight",
+    value: 8
+  }, {
+    name: "spades-nine",
+    value: 9
+  }, {
+    name: "spades-ten",
+    value: 10
+  }, {
+    name: "spades-jack",
+    value: 11
+  }, {
+    name: "spades-queen",
+    value: 12
+  }, {
+    name: "spades-king",
+    value: 13
+  }, {
+    name: "spades-ace",
+    value: 14
+  }, {       
+    name: "hearts-two", // Hearts
+    value: 2
+  }, {
+    name: "hearts-three",
+    value: 3
+  }, {
+    name: "hearts-four",
+    value: 4
+  }, {
+    name: "hearts-five",
+    value: 5
+  }, {
+    name: "hearts-six",
+    value: 6
+  }, {
+    name: "hearts-seven",
+    value: 7
+  }, {
+    name: "hearts-eight",
+    value: 8
+  }, {
+    name: "hearts-nine",
+    value: 9
+  }, {
+    name: "hearts-ten",
+    value: 10
+  }, {
+    name: "hearts-jack",
+    value: 11
+  }, {
+    name: "hearts-queen",
+    value: 12
+  }, {
+    name: "hearts-king",
+    value: 13
+  }, {
+    name: "hearts-ace",
+    value: 14
+  }, {
+    name: "diamonds-two", // Diamonds
+    value: 2
+  }, {
+    name: "diamonds-three",
+    value: 3
+  }, {
+    name: "diamonds-four",
+    value: 4
+  }, {
+    name: "diamonds-five",
+    value: 5
+  }, {
+    name: "diamonds-six",
+    value: 6
+  }, {
+    name: "diamonds-seven",
+    value: 7
+  }, {
+    name: "diamonds-eight",
+    value: 8
+  }, {
+    name: "diamonds-nine",
+    value: 9
+  }, {
+    name: "diamonds-ten",
+    value: 10
+  }, {
+    name: "diamonds-jack",
+    value: 11
+  }, {
+    name: "diamonds-queen",
+    value: 12
+  }, {
+    name: "diamonds-king",
+    value: 13
+  }, {
+    name: "diamonds-ace",
+    value: 14
+  }, {
+    name: "clubs-two", // Clubs
+    value: 2
+  }, {
+    name: "clubs-three",
+    value: 3
+  }, {
+    name: "clubs-four",
+    value: 4
+  }, {
+    name: "clubs-five",
+    value: 5
+  }, {
+    name: "clubs-six",
+    value: 6
+  }, {
+    name: "clubs-seven",
+    value: 7
+  }, {
+    name: "clubs-eight",
+    value: 8
+  }, {
+    name: "clubs-nine",
+    value: 9
+  }, {
+    name: "clubs-ten",
+    value: 10
+  }, {
+    name: "clubs-jack",
+    value: 11
+  }, {
+    name: "clubs-queen",
+    value: 12
+  }, {
+    name: "clubs-king",
+    value: 13
+  }, {
+    name: "clubs-ace",
+    value: 14
+  }
+]
+
+
+
+
+class Player {
+	constructor() {
+
+	}
+
+//methods that define Player
+
+}
+
+
+
+
+
+
+
+
+
+
+
+//object
+const game = {
+
+
+
+
+
+//methods that define game
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
